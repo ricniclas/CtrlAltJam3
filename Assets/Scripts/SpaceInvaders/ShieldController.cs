29a8ccfa08ad2252af43c999a260e028046a3fb3
unknown <luisocara.luis@gmail.com>
2024-05-12 13:46:43 -0300

    rotation

---
+++ Assets/Scripts/SpaceInvaders/ShieldController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+namespace CtrlAltJam3
+{
+    public class ShieldController : MonoBehaviour
+    {
+        private Vector2 moveInput;
+        private Rigidbody2D rb;
+        public float rotationSpeed = 100f;
+
+        // Start is called before the first frame update
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+            float rotationAmount = -moveInput.x * rotationSpeed * Time.deltaTime; 
+            transform.Rotate(0, 0, rotationAmount);
+        }
+
+
+        void OnMove(InputValue value)
+        {
+            Debug.Log("Foi" + moveInput);
+            moveInput = value.Get<Vector2>();
+
+        }
+
+     
+    }
+}
